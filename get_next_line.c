80df8f5e911d7672279526114310becea8a45be5
emmmilla <emmmilla@student.42madrid.com>
2026-06-23 12:49:24 +0200

    Add get_next_line.c

---
+++ get_next_line.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emmmilla <emmmilla@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/23 12:27:44 by emmmilla          #+#    #+#             */
+/*   Updated: 2026/06/23 12:47:44 by emmmilla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*get_next_line(int fd)
+{
+	static char	*cache;
+	char		*line;
+
+	cache = read_file(fd, cache);
+	if (!cache)
+		return (NULL);
+	line = extract_line(cache);
+	cache = clean_cache(cache);
+	return (line);
+}
