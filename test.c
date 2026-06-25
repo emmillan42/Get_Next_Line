9fea7ddd96135802cc072a546435b0bf5f044441
emmmilla <emmmilla@student.42madrid.com>
2026-06-25 15:49:14 +0200

    Fixing test.c

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: emmmilla <emmmilla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/25 12:20:30 by emmmilla          #+#    #+#             */
-/*   Updated: 2026/06/25 15:19:59 by emmmilla         ###   ########.fr       */
+/*   Updated: 2026/06/25 15:47:45 by emmmilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,10 +44,7 @@ int	main(int argc, char **argv)
 	}
 	fd = open(argv[1], O_RDONLY);
 	if (fd < 0)
-	{
-		perror("open");
-		return (1);
-	}
+		return (perror("open"), 1);
 	line_no = 1;
 	line = get_next_line(fd);
 	while (line)
