5d1555d1224be0582b92074b79682a619cfe5bcd
emmmilla <emmilla42@student.42madrid.com>
2026-06-25 08:59:39 +0200

    Add get_next_line.h

---
+++ get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emmmilla <emmmilla@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/24 22:23:20 by emmmilla          #+#    #+#             */
+/*   Updated: 2026/06/25 08:54:13 by emmmilla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char		*get_next_line(int fd);
+
+/*Utility Functions*/
+size_t		ft_strlen(const char *s);
+char		*ft_strdup(const char *s);
+char		*ft_strchr(const char *s, int c);
+char		*ft_substr(const char *s, unsigned int start, size_t len);
+char		*ft_strjoin(char *s1, char *s2);
+
+#endif
